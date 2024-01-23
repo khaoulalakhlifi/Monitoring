77d41e8bd70140b3d842f01867f50254046507a5
khaoulalakhlifi <157298338+khaoulalakhlifi@users.noreply.github.com>
2024-01-23 10:19:26 +0100

    Update config.py

--- Monitoring/config.py
+++ Monitoring/config.py
@@ -3,6 +3,6 @@
 class Config:
     SECRET_KEY = 'khaoula_badra'
     MONGODB_SETTINGS = {
-        'db': 'monitor_database',  # Choisissez un nom significatif
+        'db': 'monitor_database', 
         'host': 'mongodb://localhost/monitor_database'
     }
